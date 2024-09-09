9f371c95669350adb234d18260ad9dcecdbdeb7c
Cao Đức Thành <caoducthanh18@gmail.com>
2024-09-10 01:30:16 +0700

    Add project files.

---
+++ BaiTapThayGiaoTuan1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//Họ và tên : Phạm Ngọc Thuận Thanh
+//Bắt Đầu Làm Bài
+class Program
+{
+    static void Main()
+    {
+        // Yêu cầu người dùng nhập vào hai số
+        Console.Write("Nhap so thu nhat: ");
+        double soThuNhat = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Nhap so thu hai: ");
+        double soThuHai = Convert.ToDouble(Console.ReadLine());
+
+        // Tính trung bình cộng
+        double trungBinhCong = (soThuNhat + soThuHai) / 2;
+
+        // Hiển thị kết quả
+        Console.WriteLine($"Trung binh cong cua {soThuNhat} va {soThuHai} la: {trungBinhCong}");
+    }
+}
